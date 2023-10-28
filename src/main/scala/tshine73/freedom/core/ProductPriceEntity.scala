198493f358787d58d795fe4a1db872612d9a65a4
tshine73 <fan.steven.chiang@gmail.com>
2023-10-28 11:21:55 +0800

    add constants

--- src/main/scala/tshine73/freedom/core/ProductPriceEntity.scala
+++ src/main/scala/tshine73/freedom/core/ProductPriceEntity.scala
@@ -4,13 +4,45 @@ import org.joda.time.DateTime
 import software.amazon.awssdk.services.dynamodb.model.AttributeValue
 import tshine73.freedom.utils.DateUtils
 
-case class ProductPriceEntity(date: DateTime, item: String, price: Double, count: Int, unit: String, capacity: Int, location: String, category: String, promotion: Boolean):
+
+enum Units:
+  case KG, PIECE, G, TKG
+
+object Units:
+  def apply(s: String) = Units.valueOf(s)
+
+enum Location(val name: String):
+  case Costco extends Location("Costco")
+  case PxMart extends Location("全聯")
+  case Market extends Location("Market")
+  case RtMart extends Location("大潤發")
+end Location
+
+object Location:
+  def apply(s: String) = s.toLowerCase match
+    case "costco" => Costco
+    case "全聯" => PxMart
+    case "market" => Market
+    case "大潤發" => RtMart
+
+case class ProductPriceEntity(
+                               date: DateTime,
+                               item: String,
+                               price: Double,
+                               count: Int,
+                               unit: Units,
+                               location: Location,
+                               category: String,
+                               promotion: Boolean = false,
+                               capacity: Option[Int] = None,
+                               brand: Option[String] = None
+                             ):
   override def toString: String =
     f"date [$date], item [$item], price [$price]"
 
 
 object ProductPriceEntity:
-  val tableName = "ProductPrice"
+  val tableName = "Product-Price"
 
   def generateDynamodbItem(entity: ProductPriceEntity) =
     val itemValues = collection.mutable.Map.empty[String, AttributeValue]
@@ -19,11 +51,14 @@ object ProductPriceEntity:
     itemValues.put("item", AttributeValue.builder().s(entity.item).build())
     itemValues.put("price", AttributeValue.builder().n(entity.price.toString).build())
     itemValues.put("count", AttributeValue.builder().n(entity.count.toString).build())
-    itemValues.put("unit", AttributeValue.builder().s(entity.unit).build())
-    itemValues.put("capacity", AttributeValue.builder().n(entity.capacity.toString).build())
-    itemValues.put("location", AttributeValue.builder().s(entity.location).build())
+    itemValues.put("unit", AttributeValue.builder().s(entity.unit.toString).build())
+    itemValues.put("location", AttributeValue.builder().s(entity.location.name).build())
     itemValues.put("category", AttributeValue.builder().s(entity.category).build())
     itemValues.put("promotion", AttributeValue.builder().bool(entity.promotion).build())
+    itemValues.put("brand", AttributeValue.builder().s(entity.brand.getOrElse("")).build())
+
+    if entity.capacity.nonEmpty then
+      itemValues.put("capacity", AttributeValue.builder().n(entity.capacity.map(_.toString).getOrElse("")).build())
 
     itemValues.toMap
 
--- src/main/scala/tshine73/freedom/market/main.scala
+++ src/main/scala/tshine73/freedom/market/main.scala
@@ -1,8 +1,16 @@
 package tshine73.freedom.market
 
+import org.joda.time.DateTime
+import tshine73.freedom.core.{Location, ProductPriceEntity, Units}
 import tshine73.freedom.utils.GoogleSheetUtils
+import tshine73.freedom.core.Location.*
+import tshine73.freedom.utils.aws.DynamoDBUtils
 
 @main
 def main(parameters: String*): Unit =
   val priceSheetId = "1ZBHuHyJCz6OpCkpfGEZ5ADDsli58vkGJEmHLTPm0wOU"
-  println(GoogleSheetUtils.getValues(priceSheetId, "ProductPrice:A2:J20"))
+  //  println(GoogleSheetUtils.getValues(priceSheetId, "ProductPrice!A2:J20"))
+
+
+  val entity = ProductPriceEntity(DateTime.now(), "onion", 32.2, 1, Units("PIECE"), Location("Costco"), "Ingredient")
+  DynamoDBUtils.putItem(ProductPriceEntity.generateDynamodbItem(entity), ProductPriceEntity.tableName)
